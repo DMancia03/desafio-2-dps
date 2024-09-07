538e8d226c1175050f99456720da9fa3c0d366d4
DMancia03 <diego.mancia.03@gmail.com>
2024-09-07 12:44:06 -0600

    Agregando tab principal

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import colors from './src/styles/colors';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ingresos from './src/screens/datos_financieros/Ingresos';
+import Calificacion from './src/screens/calificacion_cliente/Calificacion';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveBackgroundColor: colors.PRYMARY_COLOR,
+          tabBarActiveTintColor: colors.SECONDARY_COLOR,
+          tabBarInactiveBackgroundColor: colors.SECONDARY_COLOR,
+          tabBarInactiveTintColor: colors.PRYMARY_COLOR,
+        }} >
+        <Tab.Screen 
+          name='ingresos' 
+          component={Ingresos}
+          options={{
+            title: 'Ingresos',
+            headerStyle: {
+              backgroundColor: colors.PRYMARY_COLOR,
+            },
+            headerTitleStyle: {
+              color: colors.SECONDARY_COLOR,
+            },
+            tabBarIcon: ({ color }) => <Icon  name="home" color={color} size={26} />,
+          }} />
+        <Tab.Screen 
+          name='calificacion' 
+          component={Calificacion}
+          options={{
+            title: 'Calificación',
+            headerStyle: {
+              backgroundColor: colors.PRYMARY_COLOR,
+            },
+            headerTitleStyle: {
+              color: colors.SECONDARY_COLOR,
+            },
+            tabBarIcon: ({ color }) => <Icon  name="home" color={color} size={26} />,
+          }} />
+      </Tab.Navigator>
+
       <StatusBar style="auto" backgroundColor={colors.PRYMARY_COLOR} />
-    </View>
+    </NavigationContainer>
   );
 }
 
