e828a99143be894da49b16f373af26e76bbbb2bb
DMancia03 <diego.mancia.03@gmail.com>
2024-09-07 12:34:22 -0600

    Personalizando status bar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import colors from './src/styles/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor={colors.PRYMARY_COLOR} />
     </View>
   );
 }
