20e8335180dcbceb8be846b85c75b40f1f382884
DMancia03 <diego.mancia.03@gmail.com>
2024-09-11 23:30:06 -0600

    Creando componentes para productos y tarjetas

---
+++ src/components/ProductContainer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import colors from "../styles/colors";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+
+const ProductContainer = ({product}) => {
+    return (
+        <View style={styles.productContainer}>
+            <View style={styles.productContainer_Header}>
+                <Text style={styles.productContainer_Title}>{product.title}</Text>
+            </View>
+            <View style={styles.container_body}>
+                {
+                    (product.items.length > 0) ? (
+                        product.items.map((item, index) => (
+                            <Text key={item.id}><Icon name={item.icon} size={20}/> <Text>{item.description}</Text></Text>
+                        ))
+                    ) : (
+                        <Text><Icon name={product.message_no_products.icon} size={20}/> <Text>{product.message_no_products.description}</Text></Text>
+                    )
+                }
+            </View>
+        </View>
+    );
+};
+
+export default ProductContainer;
+
+const styles = StyleSheet.create({
+    productContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap:20,
+        margin:20,
+        backgroundColor: colors.WHITE,
+        borderRadius: 15,
+        padding: 20,
+    },
+    productContainer_Header: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+    },
+    productContainer_Title: {
+        fontSize: 20,
+    },
+    container_body: {
+        display: 'flex',
+        flexDirection: 'column',
+        gap:20,
+    },
+    container_body_line: {
+        display: 'flex',
+        flexDirection: 'row',
+    }
+});
--- src/screens/calificacion_cliente/Calificacion.js
+++ src/screens/calificacion_cliente/Calificacion.js
@@ -1,37 +1,127 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, FlatList } from 'react-native';
 import colors from "../../styles/colors";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import ProductContainer from "../../components/ProductContainer";
+import CardContainer from "../../components/CardContainer";
 
 const Calificacion = ({ navigation }) => {
+    const ingresos = 1000;
+    const egresos = 500;
+    const libre = ingresos - egresos;
+    const disponibilidad_decimal = libre / egresos;
+    const disponibilidad_porcentaje = disponibilidad_decimal * 100;
+
+    const products = [
+        {
+            id: 'P001',
+            title: 'Ayuda financiera',
+            items: [{
+                id: 'P001I001',
+                icon: 'finance',
+                description: 'Financiamiento para consolidado de deudas',
+            },
+            {
+                id: 'P001I002',
+                icon: 'card-account-phone',
+                description: 'Contacto para un asesor financiero',
+            }],
+            message_no_products: {
+                icon: 'alert-circle',
+                description: 'No tienes productos aquí',
+            }
+        },
+        {
+            id: 'P002',
+            title: 'Apertura de cuenta',
+            items: [{
+                id: 'P002I001',
+                icon: 'account-cash',
+                description: 'Puedes abrir una cuenta de ahorros'
+            }],
+            message_no_products: {
+                icon: 'alert-circle',
+                description: 'No tienes productos aquí',
+            }
+        },
+        {
+            id: 'P003',
+            title: 'Crédito personal',
+            items: [{
+                id: 'P003I001',
+                icon: 'cash',
+                description: 'Crédito personal hasta $2,000.00'
+            }],
+            message_no_products: {
+                icon: 'alert-circle',
+                description: 'No tienes productos aquí',
+            }
+        }
+    ];
+
+    const cards = [{
+        id: 'C001',
+        title: 'Clásica',
+        description: 'Tarjeta de crédito clásica',
+        image: require('../../../assets/img-cards/clasica.png'),
+    },
+    {
+        id: 'C002',
+        title: 'Oro',
+        description: 'Tarjeta de crédito oro',
+        image: require('../../../assets/img-cards/oro.png'),
+    },
+    {
+        id: 'C003',
+        title: 'Platinum',
+        description: 'Tarjeta de crédito platinum',
+        image: require('../../../assets/img-cards/platinum.png'),
+    },
+    {
+        id: 'C004',
+        title: 'Black',
+        description: 'Tarjeta de crédito black',
+        image: require('../../../assets/img-cards/black.png'),
+    }];
+
     return(
-        <ScrollView style={styles.scroll_container}>
-            <View style={styles.container}>
-                <View style={styles.container_header}>
-                    <Text style={styles.container_header_text}>Consolidación de deudas</Text>
-                </View>
-            </View>
-            <View style={styles.container}>
-                <View style={styles.container_header}>
-                    <Text style={styles.container_header_text}>Apertura de cuenta</Text>
-                </View>
-            </View>
-            <View style={styles.container}>
-                <View style={styles.container_header}>
-                    <Text style={styles.container_header_text}>Crédito personal</Text>
-                </View>
-            </View>
-            <View style={styles.container}>
-                <View style={styles.container_header}>
-                    <Text style={styles.container_header_text}> Tarjetas disponibles</Text>
-                </View>
-                <Image source={require('../../../assets/img-cards/clasica.png')} style={styles.card} />
-                <Image source={require('../../../assets/img-cards/oro.png')} style={styles.card} />
-                <Image source={require('../../../assets/img-cards/platinum.png')} style={styles.card} />
-                <Image source={require('../../../assets/img-cards/black.png')} style={styles.card} />
-                <View style={styles.container_header}>
-                    <Text>No puedes optar por una tarjeta.</Text>
-                </View>
+        <ScrollView style={styles.scroll}>
+            <View style={styles.section}>
+                {
+                    products.length > 0 ? (
+                        <>
+                        <View style={styles.section_header}>
+                            <Text style={styles.section_title}>Productos</Text>
+                        </View>
+
+                        <FlatList
+                            data={products}
+                            keyExtractor={item => item.id}
+                            renderItem={({ item }) => (
+                                <ProductContainer product={item}/>
+                            )}
+                        />
+                        </>
+                    ) : null
+                }
+
+                {
+                    cards.length > 0 ? (
+                        <>
+                        <View style={styles.section_header}>
+                            <Text style={styles.section_title}>Tarjetas</Text>
+                        </View>
+
+                        <FlatList
+                            data={cards}
+                            keyExtractor={item => item.id}
+                            renderItem={({item}) => (
+                                <CardContainer card={item}/>
+                            )}
+                        />
+                    </>
+                    ) : null
+                }
             </View>
         </ScrollView>
     )
@@ -40,13 +130,14 @@ const Calificacion = ({ navigation }) => {
 export default Calificacion;
 
 const styles = StyleSheet.create({
-    scroll_container: {
+    scroll: {
+        backgroundColor: colors.GRAY_BACKGROUND,
+    },
+    section: {
         display: 'flex',
         flexDirection: 'column',
     },
     container: {
-        backgroundColor: colors.GRAY_BACKGROUND,
-        padding: 20,
         display: 'flex',
         flexDirection: 'column',
         gap: 20,
@@ -56,16 +147,29 @@ const styles = StyleSheet.create({
         height: 250,
         borderRadius: 10,
     },
-    container_header: {
+    section_header: {
         backgroundColor: colors.WHITE,
         borderRadius: 10,
         padding:20,
+        margin:20,
         display: 'flex',
         flexDirection: 'row',
         alignContent: 'center',
     },
-    container_header_text: {
+    section_title: {
         fontSize: 20,
         fontWeight: 'bold',
+    },
+    container_body: {
+        backgroundColor: colors.WHITE,
+        padding: 20,
+        borderRadius: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        gap:20,
+    },
+    container_body_line: {
+        display: 'flex',
+        flexDirection: 'row',
     }
 });
