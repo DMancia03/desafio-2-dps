39b509f2ebd01383c88fce52433639690ee23ab8
DMancia03 <diego.mancia.03@gmail.com>
2024-09-12 23:35:15 -0600

    Arreglando calculo de la disponibilidad

--- src/screens/calificacion_cliente/Calificacion.js
+++ src/screens/calificacion_cliente/Calificacion.js
@@ -1,55 +1,101 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, ScrollView, FlatList } from 'react-native';
+import React, {useState} from "react";
+import { View, Text, StyleSheet, Image, ScrollView, FlatList, Dimensions } from 'react-native';
 import colors from "../../styles/colors";
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import ProductContainer from "../../components/ProductContainer";
 import CardContainer from "../../components/CardContainer";
 import productos from "../../data/productos";
 import tarjetas from "../../data/tarjetas";
 import TitleContainer from "../../components/TitleContainer";
+import { ProgressChart } from "react-native-chart-kit";
 
 const Calificacion = ({ navigation }) => {
     const ingresos = 5000;
-    const egresos = 1000;
-    const libre = ingresos - egresos;
-    const disponibilidad_decimal = libre / egresos;
-    const disponibilidad_porcentaje = disponibilidad_decimal * 100;
+    const egresos = 190;
+    let libre = 0;
+    let disponibilidad_decimal = 0;
+    let disponibilidad_porcentaje = 0;
 
-    let products = productos.riesgo_test;
-    let cards = tarjetas.riesgo_test;
+    //Arrays principales
+    let products = productos.riesgo_default;
+    let cards = tarjetas.riesgo_default;
 
-    if(ingresos <= 0 && egresos <= 0){
-        products = productos.riesgo_default;
-        cards = tarjetas.riesgo_default;
-    }
-    else if(ingresos <= egresos){
-        products = productos.riesgo_extremo;
-        cards = tarjetas.riesgo_extremo;
-    }
-    else if(ingresos <= 360){
-        products = productos.riesgo_alto;
-        cards = tarjetas.riesgo_alto;
-    }
-    else if(ingresos > 360 && ingresos <= 700){
-        products = productos.riesgo_suficiente;
-        cards = tarjetas.riesgo_suficiente;
-    }
-    else if(ingresos > 700 && ingresos <= 1200){
-        products = productos.riesgo_bueno;
-        cards = tarjetas.riesgo_bueno;
-    }
-    else if(ingresos > 1200 && ingresos <= 3000){
-        products = productos.riesgo_muy_bueno;
-        cards = tarjetas.riesgo_muy_bueno;
-    }
-    else if(ingresos > 3000){
-        products = productos.riesgo_excelente;
-        cards = tarjetas.riesgo_excelente;
+    const setProducts = (array) => {
+        products = array;
     }
 
+    const setCards = (array) => {
+        cards = array;
+    }
+
+    //Llenar arrays con productos y tarjetas dependiendo de la calificación
+    const llenarExtremo = () => {
+        setProducts(productos.riesgo_extremo);
+        setCards(tarjetas.riesgo_extremo);
+    }
+
+    const llenarAlto = () => {
+        setProducts(productos.riesgo_alto);
+        setCards(tarjetas.riesgo_alto);
+    }
+
+    const llenarSuficiente = () => {
+        setProducts(productos.riesgo_suficiente);
+        setCards(tarjetas.riesgo_suficiente);
+    }
+
+    const llenarBueno = () => {
+        setProducts(productos.riesgo_bueno);
+        setCards(tarjetas.riesgo_bueno);
+    }
+
+    const llenarMuyBueno = () => {
+        setProducts(productos.riesgo_muy_bueno);
+        setCards(tarjetas.riesgo_muy_bueno);
+    }
+
+    const llenarExcelente = () => {
+        setProducts(productos.riesgo_excelente);
+        setCards(tarjetas.riesgo_excelente);
+    }
+
+    if(ingresos > 0 && egresos > 0){
+        libre = ingresos - egresos;
+        disponibilidad_decimal = libre.toFixed(2) / ingresos.toFixed(2);
+        disponibilidad_porcentaje = disponibilidad_decimal.toFixed(2) * 100;
+
+        if(ingresos <= egresos) llenarExtremo();
+        else if(ingresos <= 360) llenarAlto();
+        else if(ingresos > 360 && ingresos <= 700){
+            if(disponibilidad_porcentaje <= 40) llenarAlto();
+            else llenarSuficiente();
+        }
+        else if(ingresos > 700 && ingresos <= 1200){
+            if(disponibilidad_porcentaje <= 20) llenarAlto();
+            else if(disponibilidad_porcentaje > 20 && disponibilidad_porcentaje <= 40) llenarSuficiente();
+            else llenarBueno();
+        }
+        else if(ingresos > 1200 && ingresos <= 3000){
+            if(disponibilidad_porcentaje <= 20) llenarSuficiente();
+            else if(disponibilidad_porcentaje > 20 && disponibilidad_porcentaje <= 40) llenarBueno();
+            else llenarMuyBueno();
+        }
+        else if(ingresos > 3000){
+            if(disponibilidad_porcentaje <= 20) llenarBueno();
+            else if(disponibilidad_porcentaje > 20 && disponibilidad_porcentaje <= 40) llenarMuyBueno();
+            else llenarExcelente();
+        }
+    }
+    
     return(
         <ScrollView style={styles.scroll}>
             <View style={styles.section}>
+                {
+                    disponibilidad_porcentaje > 0 ? (
+                        <TitleContainer title={"Tienes una disponibiliad de " + disponibilidad_porcentaje + "%"} />
+                    ) : (
+                        <TitleContainer title={"Tienes una disponibiliad negativa :c"} />
+                    )
+                }
                 {
                     products.length > 0 ? (
                         <>
