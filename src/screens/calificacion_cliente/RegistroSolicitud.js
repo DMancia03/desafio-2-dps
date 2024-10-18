1f1d54853ad25064e77b8ba3e2c7f76bc764442a
Rodrigo_Beltrán <rodrigo.beltrn@gmail.com>
2024-10-17 20:08:35 -0600

    Agregando picker en productos y mejorando el visual

--- src/screens/calificacion_cliente/RegistroSolicitud.js
+++ src/screens/calificacion_cliente/RegistroSolicitud.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Button, Alert, ScrollView } from 'react-native';
 import { launchCamera } from 'react-native-image-picker';
+import { Picker } from '@react-native-picker/picker'; // Importa Picker
 import axios from 'axios';  // Importar Axios
 
 const RegistroSolicitud = () => {
@@ -15,12 +16,11 @@ const RegistroSolicitud = () => {
   const [ingresos, setIngresos] = useState('');
   const [egresos, setEgresos] = useState('');
   
-  // Función para tomar una foto con la cámara
   const takePhoto = async (setImage) => {
     const options = {
       mediaType: 'photo',
-      includeBase64: true, // Para guardar la imagen como base64
-      saveToPhotos: false,  // No guardar en galería
+      includeBase64: true,
+      saveToPhotos: false,
     };
 
     const result = await launchCamera(options);
@@ -30,7 +30,7 @@ const RegistroSolicitud = () => {
     } else if (result.errorMessage) {
       console.log('Error al abrir la cámara: ', result.errorMessage);
     } else {
-      setImage(result.assets[0].base64); // Guarda la imagen en formato base64
+      setImage(result.assets[0].base64);
     }
   };
 
@@ -83,77 +83,104 @@ const RegistroSolicitud = () => {
     <ScrollView style={styles.scrollContainer}>
       <View style={styles.container}>
         <Text style={styles.title}>Registro de Solicitud</Text>
-        <Text style={styles.label}>Producto:</Text>
-        <TextInput
-          placeholder="Ingrese el producto"
-          value={producto}
-          onChangeText={setProducto}
-          style={styles.input}
-        />
 
-        <Text style={styles.label}>Nombre Completo:</Text>
-        <TextInput
-          placeholder="Ingrese su nombre completo"
-          value={nombre}
-          onChangeText={setNombre}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Carnet (Foto):</Text>
-        <TouchableOpacity onPress={() => takePhoto(setCarnetBase64)}>
-          <View style={styles.imagePicker}>
-            <Text style={styles.imageText}>{carnetBase64 ? 'Foto tomada' : 'Tomar foto del Carnet'}</Text>
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Producto:</Text>
+          <View style={styles.pickerContainer}>
+            <Picker
+              selectedValue={producto}
+              onValueChange={(itemValue) => setProducto(itemValue)}
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleccione un producto" value="" />
+              <Picker.Item label="Apertura de cuenta" value="Apertura de cuenta" />
+              <Picker.Item label="Crédito personal" value="Crédito personal" />
+              <Picker.Item label="Tarjetas" value="Tarjetas" />
+            </Picker>
           </View>
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Nombre Completo:</Text>
+          <TextInput
+            placeholder="Ingrese su nombre completo"
+            value={nombre}
+            onChangeText={setNombre}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Carnet (Foto):</Text>
+          <TouchableOpacity onPress={() => takePhoto(setCarnetBase64)}>
+            <View style={styles.imagePicker}>
+              <Text style={styles.imageText}>{carnetBase64 ? 'Foto tomada' : 'Tomar foto del Carnet'}</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Selfie:</Text>
+          <TouchableOpacity onPress={() => takePhoto(setSelfieBase64)}>
+            <View style={styles.imagePicker}>
+              <Text style={styles.imageText}>{selfieBase64 ? 'Selfie tomada' : 'Tomar una Selfie'}</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Teléfono:</Text>
+          <TextInput
+            placeholder="Ingrese su número de teléfono"
+            value={telefono}
+            onChangeText={setTelefono}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Dirección:</Text>
+          <TextInput
+            placeholder="Ingrese su dirección"
+            value={direccion}
+            onChangeText={setDireccion}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Token Push:</Text>
+          <TextInput
+            placeholder="Token Push"
+            value={tokenPush}
+            onChangeText={setTokenPush}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Ingresos:</Text>
+          <TextInput
+            placeholder="Ingrese sus ingresos"
+            value={ingresos}
+            onChangeText={setIngresos}
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Egresos:</Text>
+          <TextInput
+            placeholder="Ingrese sus egresos"
+            value={egresos}
+            onChangeText={setEgresos}
+            style={styles.input}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Enviar Solicitud</Text>
         </TouchableOpacity>
-
-        <Text style={styles.label}>Selfie:</Text>
-        <TouchableOpacity onPress={() => takePhoto(setSelfieBase64)}>
-          <View style={styles.imagePicker}>
-            <Text style={styles.imageText}>{selfieBase64 ? 'Selfie tomada' : 'Tomar una Selfie'}</Text>
-          </View>
-        </TouchableOpacity>
-
-        <Text style={styles.label}>Teléfono:</Text>
-        <TextInput
-          placeholder="Ingrese su número de teléfono"
-          value={telefono}
-          onChangeText={setTelefono}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Dirección:</Text>
-        <TextInput
-          placeholder="Ingrese su dirección"
-          value={direccion}
-          onChangeText={setDireccion}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Token Push:</Text>
-        <TextInput
-          placeholder="Token Push"
-          value={tokenPush}
-          onChangeText={setTokenPush}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Ingresos:</Text>
-        <TextInput
-          placeholder="Ingrese sus ingresos"
-          value={ingresos}
-          onChangeText={setIngresos}
-          style={styles.input}
-        />
-
-        <Text style={styles.label}>Egresos:</Text>
-        <TextInput
-          placeholder="Ingrese sus egresos"
-          value={egresos}
-          onChangeText={setEgresos}
-          style={styles.input}
-        />
-
-        <Button title="Enviar Solicitud" onPress={handleSubmit} />
       </View>
     </ScrollView>
   );
@@ -162,42 +189,82 @@ const RegistroSolicitud = () => {
 const styles = StyleSheet.create({
   scrollContainer: {
     flex: 1,
+    backgroundColor: '#f7f8fa', // Fondo claro y suave
   },
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#ffffff',
   },
   title: {
-    fontSize: 24,
+    fontSize: 26,
     fontWeight: 'bold',
     marginBottom: 20,
     textAlign: 'center',
     color: '#003366',
   },
+  formGroup: {
+    marginBottom: 20,
+  },
   label: {
-    marginBottom: 5,
+    fontSize: 16,
     fontWeight: 'bold',
+    marginBottom: 8,
     color: '#003366',
   },
   input: {
-    height: 40,
+    height: 45,
     borderColor: '#cccccc',
     borderWidth: 1,
-    borderRadius: 5,
-    marginBottom: 15,
-    paddingHorizontal: 10,
+    borderRadius: 8,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 2, // Elevación para Android
+  },
+  pickerContainer: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 2,
+  },
+  picker: {
+    height: 45,
   },
   imagePicker: {
-    height: 40,
+    height: 45,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#e0e0e0',
-    borderRadius: 5,
-    marginBottom: 15,
+    borderRadius: 8,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 2,
   },
   imageText: {
     color: '#555',
+  },
+  button: {
+    backgroundColor: '#004080',
+    paddingVertical: 15,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
